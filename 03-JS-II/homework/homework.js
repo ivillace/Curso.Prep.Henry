b95571de1da343dd04c211627669c008d8beca4f
roberT-23 <roberbaldoovino167@gmail.com>
2022-01-12 06:42:43 -0500

    de numeros primos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -183,6 +183,12 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+   var multiplico;
+   var array[]
+   for (lef i = 0; i<=10; i++) {
+     array.push(multiplico*i)
+     
+   }return array
   
 }
 
